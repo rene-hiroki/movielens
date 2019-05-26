3c08aa4140bb5a1224c05ca18b28da06d2a8e88a
Akiyama Hiroki <hiroki.akiyama.tokyo@gmail.com>
2019-05-26 21:04:22 +0900

    brush up code and write report

--- script.R
+++
@@ -1,9 +0,0 @@
-library(caret)
-library(dplyr)
-library(purrr)
-library(ggplot2)
-library(tidyr)
-edx <- readRDS("C:/R/Rproject/movielens/edx.rds")
-validation <- readRDS("C:/R/Rproject/movielens/validation.rds")
-
-str(edx)
